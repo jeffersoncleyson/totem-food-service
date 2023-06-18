4841c584b054497aebee59de9029e913f047d60f
Jefferson Cleyson Gomes Almeida <jefferson.almeida@sensedia.com>
2023-06-18 17:14:10 -0300

    chore: Remoção da anotação @Component dos Mpappers 'ICategoryMapper' e 'ICustomerMapper'

--- totem-food-application/src/main/java/com/totem/food/application/ports/in/mappers/category/ICategoryMapper.java
+++ totem-food-application/src/main/java/com/totem/food/application/ports/in/mappers/category/ICategoryMapper.java
@@ -5,12 +5,10 @@ import com.totem.food.application.ports.in.dtos.category.CategoryDto;
 import com.totem.food.domain.category.CategoryDomain;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
-import org.springframework.stereotype.Component;
 
 @Mapper(componentModel = "spring",
         unmappedSourcePolicy = ReportingPolicy.IGNORE,
         unmappedTargetPolicy = ReportingPolicy.IGNORE)
-@Component
 public interface ICategoryMapper {
 
     CategoryDomain toDomain(CategoryCreateDto input);
