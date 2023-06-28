d3fdab8aa98222d92f4eb41ef392332b1508fa5f
Jefferson Cleyson Gomes Almeida <jefferson.almeida@sensedia.com>
2023-06-28 20:49:31 -0300

    refact: Ajustes no OrderUpdateDto para adicionar quantidade de produtos

--- totem-food-application/src/main/java/com/totem/food/application/ports/in/dtos/order/totem/OrderUpdateDto.java
+++ totem-food-application/src/main/java/com/totem/food/application/ports/in/dtos/order/totem/OrderUpdateDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.apache.commons.collections4.CollectionUtils;
 
 import java.util.List;
 
@@ -13,6 +14,10 @@ import java.util.List;
 @NoArgsConstructor
 public class OrderUpdateDto {
 
-    private List<String> products;
-    private List<String> combos;
+    private List<ItemQuantityDto> products;
+    private List<ItemQuantityDto> combos;
+
+    public boolean isOrderValid(){
+        return CollectionUtils.isNotEmpty(products) || CollectionUtils.isNotEmpty(combos);
+    }
 }
