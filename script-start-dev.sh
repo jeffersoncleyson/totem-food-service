d155d49712dba9217334f21fc38e4204ccb321dc
Jefferson Cleyson Gomes Almeida <jefferson.almeida@sensedia.com>
2023-06-28 20:33:58 -0300

    chore: Adicionado 'payment-gateway' no script de start-dev

--- script-start-dev.sh
+++ script-start-dev.sh
@@ -1,7 +1,7 @@
 if [ "${1}" = "dev" ]
 then
   echo Starting only MongoDB
-  docker-compose -f docker-compose.yaml up -d mongo-dev
+  docker-compose -f docker-compose.yaml up -d mongo-dev payment-gateway
 else
   echo Starting MongoDB and Totem Food Service
   docker-compose -f docker-compose.yaml up -d --build
