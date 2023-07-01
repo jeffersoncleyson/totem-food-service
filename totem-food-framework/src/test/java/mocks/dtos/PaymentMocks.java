5161e76b7ebc07338192d15e5c8757070fe16cc5
Jefferson Cleyson Gomes Almeida <jefferson.almeida@sensedia.com>
2023-07-01 16:52:31 -0300

    test: coverage '**/framework/adapters/in/rest/payment/TotemPaymentRestApiAdapter.java'

---
+++ totem-food-framework/src/test/java/mocks/dtos/PaymentMocks.java
@@ -0,0 +1,42 @@
+package mocks.dtos;
+
+import com.totem.food.application.ports.in.dtos.payment.PaymentCreateDto;
+import com.totem.food.application.ports.in.dtos.payment.PaymentDto;
+import com.totem.food.application.ports.in.dtos.payment.PaymentQRCodeDto;
+import com.totem.food.domain.payment.PaymentDomain;
+import lombok.NoArgsConstructor;
+
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+@NoArgsConstructor
+public class PaymentMocks {
+
+    public static PaymentCreateDto paymentCreateDto(String orderId, String customerId) {
+        return new PaymentCreateDto(
+                orderId,
+                customerId
+        );
+    }
+
+    public static PaymentQRCodeDto paymentQRCodeDto(String paymentId, String status) {
+        return new PaymentQRCodeDto(
+                "qrcodeBase64",
+                "qrcode",
+                status,
+                paymentId
+        );
+    }
+
+    public static PaymentDto paymentDto(){
+        return new PaymentDto(
+                UUID.randomUUID().toString(),
+                25.0D,
+                PaymentDomain.PaymentStatus.COMPLETED.key,
+                ZonedDateTime.now(ZoneOffset.UTC),
+                ZonedDateTime.now(ZoneOffset.UTC)
+        );
+    }
+
+}
