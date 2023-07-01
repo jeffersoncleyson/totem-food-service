707b4e7d8e56256745350e50d2e1800f3da32997
wallapsantos <wallanpereira09@gmail.com>
2023-07-01 00:57:14 -0300

    style, test: inclusão de testes unitários

---
+++ totem-food-framework/src/test/java/com/totem/food/framework/adapters/in/rest/order/totem/TotemOrderRestApiAdapterTest.java
@@ -0,0 +1,139 @@
+package com.totem.food.framework.adapters.in.rest.order.totem;
+
+import com.totem.food.application.ports.in.dtos.combo.ComboDto;
+import com.totem.food.application.ports.in.dtos.order.totem.*;
+import com.totem.food.application.ports.in.dtos.product.ProductDto;
+import com.totem.food.application.usecases.commons.ICreateUseCase;
+import com.totem.food.application.usecases.commons.ISearchUseCase;
+import com.totem.food.application.usecases.commons.IUpdateStatusUseCase;
+import com.totem.food.application.usecases.commons.IUpdateUseCase;
+import com.totem.food.framework.test.utils.TestUtils;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
+class TotemOrderRestApiAdapterTest {
+
+    private final String ENDPOINT = "/v1/totem/order";
+
+    private MockMvc mockMvc;
+
+    private AutoCloseable autoCloseable;
+
+    @Mock
+    private ICreateUseCase<OrderCreateDto, OrderDto> iCreateUseCase;
+
+    @Mock
+    private ISearchUseCase<OrderFilterDto, List<OrderDto>> iSearchProductUseCase;
+
+    @Mock
+    private IUpdateUseCase<OrderUpdateDto, OrderDto> iUpdateUseCase;
+
+    @Mock
+    private IUpdateStatusUseCase<OrderDto> iUpdateStatusUseCase;
+
+    private TotemOrderRestApiAdapter totemOrderRestApiAdapter;
+
+    @BeforeEach
+    void setUp() {
+        autoCloseable = MockitoAnnotations.openMocks(this);
+        this.totemOrderRestApiAdapter = new TotemOrderRestApiAdapter(iCreateUseCase, iSearchProductUseCase, iUpdateUseCase, iUpdateStatusUseCase);
+        mockMvc = MockMvcBuilders.standaloneSetup(totemOrderRestApiAdapter).build();
+    }
+
+    @SneakyThrows
+    @AfterEach
+    void tearDown() {
+        autoCloseable.close();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ENDPOINT)
+    void create(String endpoint) throws Exception {
+
+        //## Mock - Object
+        var orderCreateDto = new OrderCreateDto();
+        orderCreateDto.setCustomerId("123");
+        orderCreateDto.setCombos(List.of(new ItemQuantityDto(1, "combo")));
+        orderCreateDto.setProducts(List.of(new ItemQuantityDto(1, "produto")));
+
+        var comboDto = new ComboDto();
+        comboDto.setName("Casa");
+        comboDto.setPrice(BigDecimal.valueOf(25.0));
+
+        var orderDto = new OrderDto();
+        orderDto.setId("1");
+        orderDto.setCustomerId("123");
+        orderDto.setProducts(List.of(ProductDto.builder().id("1").build()));
+        orderDto.setCombos(List.of(comboDto));
+        orderDto.setStatus("NEW");
+        orderDto.setPrice(25.0);
+
+        //## Given
+        when(iCreateUseCase.createItem(any(OrderCreateDto.class))).thenReturn(orderDto);
+
+        final var jsonOpt = TestUtils.toJSON(orderCreateDto);
+        final var json = jsonOpt.orElseThrow();
+        final var httpServletRequest = post(endpoint)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        //### When
+        final var resultActions = mockMvc.perform(httpServletRequest);
+
+        //### Then
+        resultActions.andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+
+        final var result = resultActions.andReturn();
+        final var responseJson = result.getResponse().getContentAsString();
+        final var orderDtoResponseOpt = TestUtils.toObject(responseJson, OrderDto.class);
+        final var orderDtoResponse = orderDtoResponseOpt.orElseThrow();
+
+        assertThat(orderDto)
+                .usingRecursiveComparison()
+                .ignoringFieldsOfTypes(ZonedDateTime.class)
+                .isNotNull();
+
+        verify(iCreateUseCase, times(1)).createItem(any(OrderCreateDto.class));
+    }
+
+
+    @Test
+    void listAll() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void testUpdate() {
+    }
+}
