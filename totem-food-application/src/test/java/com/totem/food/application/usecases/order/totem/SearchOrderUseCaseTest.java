2aee24315762b9ca073c1691f0391164753c54a0
wallapsantos <wallanpereira09@gmail.com>
2023-08-20 02:30:11 -0300

    test: teste unidade SearchOrderUseCase

--- totem-food-application/src/test/java/com/totem/food/application/usecases/order/totem/SearchOrderUseCaseTest.java
+++ totem-food-application/src/test/java/com/totem/food/application/usecases/order/totem/SearchOrderUseCaseTest.java
@@ -1,5 +1,6 @@
 package com.totem.food.application.usecases.order.totem;
 
+import com.totem.food.application.ports.in.dtos.order.totem.OrderDto;
 import com.totem.food.application.ports.in.dtos.order.totem.OrderFilterDto;
 import com.totem.food.application.ports.in.mappers.order.totem.IOrderMapper;
 import com.totem.food.application.ports.out.persistence.commons.ISearchRepositoryPort;
@@ -22,7 +23,10 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class SearchOrderUseCaseTest {
@@ -70,4 +74,41 @@ class SearchOrderUseCaseTest {
         verify(iOrderMapper, times(1)).toDto(any(OrderModel.class));
 
     }
+
+    @Test
+    void sortedOrderByStatusWhenSortedFlagIsTrue() {
+
+        var filter = OrderFilterDto.builder().customerId("123").sorted(true).build();
+
+        var order1 = createOrder("1", "NEW");
+        var order2 = createOrder("2", "READY");
+        var order3 = createOrder("3", "RECEIVED");
+        var order4 = createOrder("4", "IN_PREPARATION");
+        var order5 = createOrder("5", "FINALIZED");
+
+        List<OrderDto> orderDtos = List.of(order1, order2, order3, order4, order5);
+
+        when(iSearchOrderRepositoryPort.findAll(filter)).thenReturn(List.of(
+                OrderModel.builder().id("1").status(OrderStatusEnumDomain.NEW).build(),
+                OrderModel.builder().id("2").status(OrderStatusEnumDomain.READY).build(),
+                OrderModel.builder().id("3").status(OrderStatusEnumDomain.RECEIVED).build(),
+                OrderModel.builder().id("4").status(OrderStatusEnumDomain.IN_PREPARATION).build(),
+                OrderModel.builder().id("4").status(OrderStatusEnumDomain.FINALIZED).build()));
+
+        var result = searchOrderUseCase.items(filter);
+
+        assertNotNull(result);
+        assertEquals(3, result.size());
+        assertEquals(orderDtos.get(1).getStatus(), result.get(0).getStatus());
+
+        verify(iSearchOrderRepositoryPort, times(1)).findAll(filter);
+    }
+
+    private OrderDto createOrder(String id, String status) {
+        var order = new OrderDto();
+        order.setId(id);
+        order.setStatus(status);
+        return order;
+    }
+
 }
