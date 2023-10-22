14a48bd951a650af970ca1a1726cf55330496ec4
wallapsantos <wallanpereira09@gmail.com>
2023-10-22 12:26:04 -0300

    test: Add new unit test Totem..Adapter and CreateImage..Usecase

--- totem-food-framework/src/test/java/com/totem/food/framework/adapters/in/rest/customer/totem/TotemCustomerRestApiAdapterTest.java
+++ totem-food-framework/src/test/java/com/totem/food/framework/adapters/in/rest/customer/totem/TotemCustomerRestApiAdapterTest.java
@@ -8,7 +8,6 @@ import com.totem.food.application.usecases.commons.ICreateUseCase;
 import com.totem.food.application.usecases.commons.IDeleteUseCase;
 import com.totem.food.framework.test.utils.TestUtils;
 import lombok.SneakyThrows;
-import org.bson.types.ObjectId;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,7 +18,6 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.cloud.openfeign.security.OAuth2AccessTokenInterceptor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -28,13 +26,20 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
+import java.util.UUID;
 
+import static com.totem.food.framework.adapters.in.rest.constants.Routes.API_VERSION_1;
+import static com.totem.food.framework.adapters.in.rest.constants.Routes.CONFIRM_CUSTOMER;
+import static com.totem.food.framework.adapters.in.rest.constants.Routes.CUSTOMER_ID;
+import static com.totem.food.framework.adapters.in.rest.constants.Routes.TOTEM_CUSTOMER;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -69,7 +74,7 @@ class TotemCustomerRestApiAdapterTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = "/v1/totem/customer")
+    @ValueSource(strings = API_VERSION_1 + TOTEM_CUSTOMER)
     void createCustomer(String endpoint) throws Exception {
 
         //### Given - Objects and Values
@@ -129,7 +134,7 @@ class TotemCustomerRestApiAdapterTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = "/v1/totem/customer")
+    @ValueSource(strings = API_VERSION_1 + TOTEM_CUSTOMER)
     void deleteCustomer(String endpoint) throws Exception {
 
         //### Given - Mocks
@@ -149,5 +154,34 @@ class TotemCustomerRestApiAdapterTest {
                 .andExpect(status().isNoContent());
 
         verify(iDeleteUseCase, times(1)).removeItem(Mockito.anyString());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = API_VERSION_1 + TOTEM_CUSTOMER + CUSTOMER_ID + CONFIRM_CUSTOMER)
+    void confirm(String endpoint) throws Exception {
+
+        //### Given - Objects and Values
+        final var cpf = "51397955074";
+
+        var customerConfirmDto = new CustomerConfirmDto();
+        customerConfirmDto.setCpf(cpf);
+        customerConfirmDto.setCode(UUID.randomUUID().toString());
+
+        //### Given - Mocks
+        final var jsonOpt = TestUtils.toJSON(customerConfirmDto);
+        final var json = jsonOpt.orElseThrow();
+        final var httpServletRequest = put(endpoint, customerConfirmDto.getCpf(), customerConfirmDto.getCode())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        //### When
+        final var resultActions = mockMvc.perform(httpServletRequest);
+
+        //### Then
+        resultActions.andDo(print())
+                .andExpect(status().isNoContent());
+
+        verify(iConfirmUseCase, times(1)).confirm(any(CustomerConfirmDto.class));
+
     }
 }
